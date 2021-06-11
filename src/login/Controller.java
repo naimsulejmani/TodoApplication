3df51b0035d99451e494f3ac698c79e3d96cc331
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-06-11 18:40:39 +0200

    login

--- src/login/Controller.java
+++ src/login/Controller.java
@@ -1,4 +1,29 @@
 package login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    public void login(ActionEvent event) {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if (username.isEmpty() || username.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Missing information");
+            alert.setContentText("Username is not specified!!!");
+            alert.show();
+            return;
+        }
+    }
 }
