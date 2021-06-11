41ca3cbafbcea0eea3a32e7bffe0ca1636894e94
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-06-11 18:57:03 +0200

    Open new stage and scene from login to main form

--- src/login/Controller.java
+++ src/login/Controller.java
@@ -2,9 +2,17 @@ package login;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -25,5 +33,41 @@ public class Controller {
             alert.show();
             return;
         }
+
+        if (password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Missing information");
+            alert.setContentText("Password is not specified!!!");
+            alert.show();
+            return;
+        }
+
+        if (username.equals(password)) {
+            Node source = (Node) event.getSource();
+            source.getScene().getWindow().hide();
+            loadMainForm("/mainform/mainform.fxml", "Main Todo Application Form");
+            //ne rregull
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Not authorized");
+            alert.setContentText("Username or password is wrong!!!");
+            alert.show();
+            return;
+        }
     }
+
+    private void loadMainForm(String location, String title) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(location));
+            Scene mainScene = new Scene(root);
+            Stage mainStage = new Stage(StageStyle.DECORATED);
+            mainStage.setScene(mainScene);
+            mainStage.setTitle(title);
+            mainStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
